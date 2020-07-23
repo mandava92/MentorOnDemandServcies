1daf25f5b659a7e9df3654133c5589384ca5d1b0
Nagamani Mandava <Nagamani.Mandava@viacomcontractor.com>
2020-07-23 10:24:08 +0530

    Secuity

---
+++ user/src/main/java/com/mentorondemand/user/config/JwtUtils.java
@@ -0,0 +1,48 @@
+package com.mentorondemand.user.config;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtils {
+	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
+	
+	private String jwtSecret = "secret";
+
+	private int jwtExpirationMs= 10000;
+
+	public String generateJwtToken(Authentication authentication) {
+
+		 UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+		return Jwts.builder()
+				.setSubject((userPrincipal.getUsername()))
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.compact();
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+	}
+
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			return true;
+		} catch (Exception e) {
+			logger.error("Invalid JWT signature: {}", e.getMessage());
+		}
+
+		return false;
+	}
+}
--- user/src/main/java/com/mentorondemand/user/controller/UserController.java
+++ user/src/main/java/com/mentorondemand/user/controller/UserController.java
@@ -3,10 +3,10 @@ package com.mentorondemand.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +16,12 @@ import com.mentorondemand.user.dto.UserDTO;
 import com.mentorondemand.user.service.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value="/api/user")
 public class UserController {
 	
 	@Autowired
 	public UserService userService;
-	
-	@PostMapping
-	public ResponseEntity<UserDTO> createUser(@Validated @RequestBody UserDTO user){
-		user = userService.createUser(user);
-		return ResponseEntity.ok(user);
-	}
 	
 	@PutMapping
 	public ResponseEntity<UserDTO> updateUser(@Validated @RequestBody UserDTO user){
@@ -39,7 +34,7 @@ public class UserController {
 		userService.deleteUser(id);
 	}
 	
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{userName}")
 	public ResponseEntity<UserDTO> getUser(@PathVariable Integer id){
 		UserDTO user = userService.getUser(id);
 		return ResponseEntity.ok(user);
