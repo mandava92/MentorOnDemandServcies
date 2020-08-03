b7cf3344936acede971857556e1c9884f8370272
Your Name <you@example.com>
2020-08-03 19:21:24 +0530

    changes

---
+++ config-server/src/test/java/com/mentorondemand/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorondemand.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
